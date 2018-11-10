84b6706f799affaf7407f3c9a358de85753b4667
Mat Williams <github@matwilliams.net>
2018-11-10 19:22:17 +0000

    fixed order

--- web/models/x4-team-model.inc.php
+++ web/models/x4-team-model.inc.php
@@ -37,7 +37,7 @@ class X4TeamModel extends HaploModel {
             select
                 teams.team_id, REPLACE(teams.team_name, " | X4F.PL", "") as team_name, 
                 sum(points.event_total) as event_total, sum(points.overall_total) as overall_total, 
-                COALESCE(sum(hits.hits_total * -1), 0) as hits
+                COALESCE(sum(hits.hits_total * -1), 0) as hits, sum(points.event_total) + COALESCE(sum(hits.hits_total * -1), 0) as real_total
             from
                 teams
             left join
@@ -58,7 +58,7 @@ class X4TeamModel extends HaploModel {
             group by
                 teams.team_id, teams.team_name
             order by
-                event_total DESC
+                real_total DESC
             ', array(
                 ':season_id' => 2018,
                 ':gameweek' => $gameweek
