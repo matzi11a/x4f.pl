6f3b5350019b38b2fb7b451f22a2784abbd59f1f
Mat Williams <github@matwilliams.net>
2018-11-11 13:31:48 +0000

    fixed live points join

--- fpl-scraper/models/x4points-model.php
+++ fpl-scraper/models/x4points-model.php
@@ -37,7 +37,7 @@ class X4PointsModel extends Model {
             left join 
                 live 
             on 
-                player_picks.prem_id = live.prem_id
+                player_picks.prem_id = live.prem_id and player_picks.gameweek = live.gameweek
             where 
                 live.gameweek = :gameweek
             and
